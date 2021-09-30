968add8f4473df6391ecf1b84635fb880bfe4d7c
nickpazfernande <nickpazfernandez@gmail.com>
2021-09-30 11:41:19 -0300

    Change NavBar mobile
    Ahora en pantallas de menos de 1000px el menu pasa a ser un NavBar superior. Se agregaron los productos relacionados en la pagina de Product Info.

--- js/init.js
+++ js/init.js
@@ -1,25 +1,31 @@
-const CATEGORIES_URL = "https://japdevdep.github.io/ecommerce-api/category/all.json";
-const PUBLISH_PRODUCT_URL = "https://japdevdep.github.io/ecommerce-api/product/publish.json";
-const CATEGORY_INFO_URL = "https://japdevdep.github.io/ecommerce-api/category/1234.json";
-const PRODUCTS_URL = "https://japdevdep.github.io/ecommerce-api/product/all.json";
-const PRODUCT_INFO_URL = "https://japdevdep.github.io/ecommerce-api/product/5678.json";
-const PRODUCT_INFO_COMMENTS_URL = "https://japdevdep.github.io/ecommerce-api/product/5678-comments.json";
+const CATEGORIES_URL =
+  "https://japdevdep.github.io/ecommerce-api/category/all.json";
+const PUBLISH_PRODUCT_URL =
+  "https://japdevdep.github.io/ecommerce-api/product/publish.json";
+const CATEGORY_INFO_URL =
+  "https://japdevdep.github.io/ecommerce-api/category/1234.json";
+const PRODUCTS_URL =
+  "https://japdevdep.github.io/ecommerce-api/product/all.json";
+const PRODUCT_INFO_URL =
+  "https://japdevdep.github.io/ecommerce-api/product/5678.json";
+const PRODUCT_INFO_COMMENTS_URL =
+  "https://japdevdep.github.io/ecommerce-api/product/5678-comments.json";
 const CART_INFO_URL = "https://japdevdep.github.io/ecommerce-api/cart/987.json";
 const CART_BUY_URL = "https://japdevdep.github.io/ecommerce-api/cart/buy.json";
 const user = undefined;
 var showSpinner = function () {
   document.getElementById("spinner-wrapper").style.display = "block";
-}
+};
 
 var hideSpinner = function () {
   document.getElementById("spinner-wrapper").style.display = "none";
-}
+};
 
 var getJSONData = function (url) {
   var result = {};
   showSpinner();
   return fetch(url)
-    .then(response => {
+    .then((response) => {
       if (response.ok) {
         return response.json();
       } else {
@@ -27,49 +33,94 @@ var getJSONData = function (url) {
       }
     })
     .then(function (response) {
-      result.status = 'ok';
+      result.status = "ok";
       result.data = response;
       hideSpinner();
       return result;
     })
     .catch(function (error) {
-      result.status = 'error';
+      result.status = "error";
       result.data = error;
       hideSpinner();
       return result;
     });
-}
+};
 function signOut() {
-
   var auth2 = gapi.auth2.getAuthInstance();
   auth2.signOut().then(function () {
-    console.log('User signed out.');
+    console.log("User signed out.");
   });
   window.location.replace("https://nickpazfernande.github.io/e-comercio");
 }
 function getUser() {
-  document.getElementById('idGoogle').innerHTML = localStorage.getItem('userLocal');
+  document.getElementById("idGoogle").innerHTML =
+    localStorage.getItem("userLocal");
 }
 
-function SignOutTotal(){
+function SignOutTotal() {
   removeUser();
   signOut();
 }
 function removeUser() {
-  localStorage.removeItem('userLocal');
-  localStorage.removeItem('userGoogle');
+  localStorage.removeItem("userLocal");
+  localStorage.removeItem("userGoogle");
 }
 
 function verLogin() {
-  if (window.localStorage.getItem("userLocal") === null && window.localStorage.getItem("userGoogle") === null ) {
-    
+  if (
+    window.localStorage.getItem("userLocal") === null &&
+    window.localStorage.getItem("userGoogle") === null
+  ) {
     window.location.replace("index.html");
-    
   }
+}
+
+function addNavBar() {
+  let htmlContentToAppend = `<nav class="navbar navbar-expand-lg sticky-top navbar-dark bg-dark p-3">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">e-Mercado</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="categories.html">Categorías</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="products.html">Productos</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="sell.html">Vender</a>
+        </li>
+        <li class="nav-item dropdown navbar-dark bg-dark text-white">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            NombreUser
+          </a>
+          <ul class="dropdown-menu navbar-dark bg-dark text-white " aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item text-white" href="cart.html">Ver Carrito</a></li>
+            <li><a class="dropdown-item text-white" href="my-profile.html">Mi Perfil</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item text-white" onclick="SignOutTotal()" href="index.html">Cerrar sesión</a></li>
+          </ul>
+        </li>
+      </ul> 
+    </div>
+  </div>
+</nav>
+  `;
+
+  document.getElementById("navBar").innerHTML = htmlContentToAppend;
+  document.getElementById("navbarDropdown").innerHTML =
+    localStorage.getItem("userLocal");
 }
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
+
 document.addEventListener("DOMContentLoaded", function (e) {
   //Al iniciar todas las paginas menos el login, se ejecutan estas dos funciones.
 
@@ -78,4 +129,7 @@ document.addEventListener("DOMContentLoaded", function (e) {
 
   //Verificamos si existe usuario logueado sino rediriguimos al login.
   verLogin();
+
+  //Cargamos el menu superior para pantallas menores a 1000px.
+  addNavBar();
 });
--- js/product-info.js
+++ js/product-info.js
@@ -1,6 +1,7 @@
-//listas vacias para luego agregarle la informacion desde los json. 
+//listas vacias para luego agregarle la informacion desde los json.
 var productInfo = [];
 var comments = [];
+var arrayProduct = [];
 
 //evento que se ejecuta al iniciar la pagina
 document.addEventListener("DOMContentLoaded", function (e) {
@@ -13,7 +14,14 @@ document.addEventListener("DOMContentLoaded", function (e) {
     }
   });
 
-  //Esta funcion tra la informacion de los comentarios. 
+  getJSONData(PRODUCTS_URL).then(function (resultObj) {
+    if (resultObj.status === "ok") {
+      arrayProduct = resultObj.data;
+      showRelated();
+    }
+  });
+
+  //Esta funcion tra la informacion de los comentarios.
   //y ejecuta showComments.
   getJSONData(PRODUCT_INFO_COMMENTS_URL).then(function (resultObj) {
     if (resultObj.status === "ok") {
@@ -25,8 +33,7 @@ document.addEventListener("DOMContentLoaded", function (e) {
 
 //Funcion que muestra la info del producto en el HTML.
 function showProductInfo() {
-
-  //Variable que carga las imagenes en un carousel. 
+  //Variable que carga las imagenes en un carousel.
   showCarousel = ` 
   <div id="carouselExampleIndicators" class="carousel slide pb-5 " data-ride="carousel">
     <ol class="carousel-indicators">
@@ -58,9 +65,9 @@ function showProductInfo() {
       <span class="sr-only">Next</span>
     </a>
   </div>
-    `
+    `;
 
-  //Variable que carga nombre y descripcion. 
+  //Variable que carga nombre y descripcion.
   showNameInfo = ` 
     <div class="mt-5 container">
       <div class="card " >
@@ -74,7 +81,7 @@ function showProductInfo() {
       </div>
   </div>
 
-    `
+    `;
 
   showMoreInfo = `
     <div class=" bg-light mb-5">
@@ -84,20 +91,18 @@ function showProductInfo() {
           <p class="card-title"> ${productInfo.description}</p>
         </div>
       </div>
-    </div>`
-
+    </div>`;
 
   document.getElementById("carousel").innerHTML = showCarousel;
   document.getElementById("info").innerHTML = showNameInfo;
   document.getElementById("moreInfo").innerHTML = showMoreInfo;
 
-  document.getElementById('titleProduct').innerHTML = productInfo.name;
+  document.getElementById("titleProduct").innerHTML = productInfo.name;
 }
 
-//Funcion que muesta los comentarios. 
+//Funcion que muesta los comentarios.
 function showComments() {
   let commentsActive = "";
-
 
   for (let i = 0; i < comments.length; i++) {
     comment = comments[i];
@@ -106,12 +111,12 @@ function showComments() {
     for (let i = 0; i < comment.score; i++) {
       start += `
       <span class="fa fa-star checked"></span>
-      `
+      `;
     }
     if (comment.score < 5) {
       for (let i = comment.score; i < 5; i++)
         startOff += `
-      <span class="fa fa-star"></span>`
+      <span class="fa fa-star"></span>`;
     }
     commentsActive += `
     <div class="card p-3">
@@ -125,40 +130,36 @@ function showComments() {
                       ${comment.description} </small></span> </div> <small> ${comment.dateTime} </small>
               </div>
             </div>  
-    `
+    `;
   }
   document.getElementById("comments").innerHTML = commentsActive;
-
 }
 
-//Funcion que muestro los comentarios agregados por el usuario. 
+//Funcion que muestro los comentarios agregados por el usuario.
 function sendComments() {
-  let comments = document.getElementById('sendComments').value;
-  var aviso = document.getElementById('aviso')
+  let comments = document.getElementById("sendComments").value;
+  var aviso = document.getElementById("aviso");
 
   start = "";
   startOff = "";
 
-  //If para verificar si no dejaron campos vacios. 
+  //If para verificar si no dejaron campos vacios.
   if (comments === "" || obtenerScore() === 0) {
-
-    //activo el aviso que recuerda al usuario no dejar campos vacios. 
+    //activo el aviso que recuerda al usuario no dejar campos vacios.
     aviso.style.display = "block";
   } else {
-
-    //si esta todo en orden se envia el comentario. Y oculto el aviso. 
+    //si esta todo en orden se envia el comentario. Y oculto el aviso.
     aviso.style.display = "none";
-
 
     for (let i = 0; i < obtenerScore(); i++) {
       start += `
     <span class="fa fa-star checked"></span>
-    `
+    `;
     }
     if (obtenerScore() < 5) {
       for (let i = obtenerScore(); i < 5; i++)
         startOff += `
-    <span class="fa fa-star"></span>`
+    <span class="fa fa-star"></span>`;
     }
 
     commentsActive = [];
@@ -168,53 +169,62 @@ function sendComments() {
             <div class="d-flex justify-content-between align-items-center">
               <div class="user d-flex flex-row align-items-center"> <span><small
                     class="font-weight-bold text-primary">
-                      ${localStorage.getItem('userLocal')} 
+                      ${localStorage.getItem("userLocal")} 
                      ${start}
                      ${startOff}
                       </small> </br> <small class="font-weight-bold">
                     ${comments}</small></span> </div> <small> ${obtenerFechaActual()} </small>
             </div>
           </div>  
-  `
+  `;
 
     document.getElementById("comments").innerHTML += commentsActive;
-
   }
 }
 
-//Funcion para obtener la fecha actual en formato adecuado. 
+//Funcion para obtener la fecha actual en formato adecuado.
 function obtenerFechaActual() {
   let date = new Date();
-  let day = date.getDate().toString().padStart(2, '0');
-  let month = (date.getMonth() + 1).toString().padStart(2, '0');
-  let year = date.getFullYear().toString().padStart(2, '0');
-  var hour = date.getHours().toString().padStart(2, '0');
-  var minutes = date.getMinutes().toString().padStart(2, '0');
-  var seconds = date.getSeconds().toString().padStart(2, '0');
+  let day = date.getDate().toString().padStart(2, "0");
+  let month = (date.getMonth() + 1).toString().padStart(2, "0");
+  let year = date.getFullYear().toString().padStart(2, "0");
+  var hour = date.getHours().toString().padStart(2, "0");
+  var minutes = date.getMinutes().toString().padStart(2, "0");
+  var seconds = date.getSeconds().toString().padStart(2, "0");
   //se le da el formato deseado
   var dateTime = `${year}-${month}-${day} ${hour}:${minutes}:${seconds} `;
   return dateTime;
 }
 
-
-//Funcion para obtener la cantidad de estrellas de la calificacion. 
+//Funcion para obtener la cantidad de estrellas de la calificacion.
 function obtenerScore() {
   var estrella1 = document.getElementById("radio5");
   var estrella2 = document.getElementById("radio4");
   var estrella3 = document.getElementById("radio3");
   var estrella4 = document.getElementById("radio2");
   var estrella5 = document.getElementById("radio1");
-  var score = 0
-  if (estrella5.checked)
-    score = 5
-  else if (estrella4.checked)
-    score = 4
-  else if (estrella3.checked)
-    score = 3
-  else if (estrella2.checked)
-    score = 2
-  else if (estrella1.checked)
-    score = 1
+  var score = 0;
+  if (estrella5.checked) score = 5;
+  else if (estrella4.checked) score = 4;
+  else if (estrella3.checked) score = 3;
+  else if (estrella2.checked) score = 2;
+  else if (estrella1.checked) score = 1;
 
   return score;
+}
+
+function showRelated() {
+  const related = productInfo.relatedProducts;
+  let htmlContentToAppend = `<div class="col-md-2"></div>`;
+  for (let i = 0; i < related.length; i++) {
+    htmlContentToAppend += `<div class="card m-3" style="width: 18rem;">
+    <img src=${arrayProduct[related[i]].imgSrc} class="card-img-top" alt="...">
+    <div class="card-body">
+    <h5 class="card-title">${arrayProduct[related[i]].name}</h5>
+      <p class="card-text">${arrayProduct[related[i]].description}</p>
+    </div>
+  </div>`;
+  }
+
+  document.getElementById("related").innerHTML = htmlContentToAppend;
 }
