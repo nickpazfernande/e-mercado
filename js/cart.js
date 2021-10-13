415c025976c6f038e6cdcc56be09338b64f67372
nickpazfernande <nickpazfernandez@gmail.com>
2021-10-13 16:44:03 -0300

    First commit Cart

--- js/cart.js
+++ js/cart.js
@@ -1,6 +1,24 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
+let USD = 0;
+let carrito = [];
 
+document.addEventListener("DOMContentLoaded", function (e) {
+  getJSONData(CURRENCY_URL).then(function (resultObj) {
+    if (resultObj.status === "ok") {
+      USD = (resultObj.data.rates.USD.sell + resultObj.data.rates.USD.buy) / 2;
+      showCurrency();
+    }
+  });
+  getJSONData(CART_INFO_URL).then(function (resultObj) {
+    if (resultObj.status === "ok") {
+      console.log(resultObj.data);
+    }
+  });
 });
+
+function showCurrency() {
+  console.log(USD);
+  document.getElementById("usd").innerHTML = USD;
+}
--- js/init.js
+++ js/init.js
@@ -10,8 +10,11 @@ const PRODUCT_INFO_URL =
   "https://japdevdep.github.io/ecommerce-api/product/5678.json";
 const PRODUCT_INFO_COMMENTS_URL =
   "https://japdevdep.github.io/ecommerce-api/product/5678-comments.json";
-const CART_INFO_URL = "https://japdevdep.github.io/ecommerce-api/cart/987.json";
+const CART_INFO_URL = "https://japdevdep.github.io/ecommerce-api/cart/654.json";
 const CART_BUY_URL = "https://japdevdep.github.io/ecommerce-api/cart/buy.json";
+const CURRENCY_URL =
+  "https://cotizaciones-brou.herokuapp.com/api/currency/latest";
+
 const user = undefined;
 var showSpinner = function () {
   document.getElementById("spinner-wrapper").style.display = "block";
